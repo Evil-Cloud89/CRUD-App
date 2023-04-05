aca0df75094d80e5365cf0749d096002958b9127
Mateusz Oko <mo89mo@gmail.com>
2023-04-05 17:41:17 +0200

    Add initialState and Posts

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,19 @@
+import { Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Posts from '../../features/Posts/Posts';
+
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  return(
+    <div>  
+        <Row>
+            <Col className="d-flex justify-content-between">
+                <h1>All posts</h1>
+                <Button className="mb-3" variant='outline-primary' as={Link} to={'/post/add' }>Add post</Button>
+            </Col>
+        </Row>
+        <Posts/>
+    </div>
+  );
+};
 
 export default Home;
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,38 @@
 const initialState = {
-    posts: [],
+    posts: [
+        {
+            id: '1',
+            title: 'Article title',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '02-02-2022',
+            author: 'John Doe'
+        },
+        {
+            id: '2',
+            title: 'Article title II',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '17-01-2023',
+            author: 'Mick Autumn'
+        },
+        {
+            id: '3',
+            title: 'Article title III',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '25-07-2022',
+            author: 'Joe Smith'
+        },
+        {
+            id: '4',
+            title: 'Article title IV',
+            shortDescription: 'Short description of the article...',
+            content: 'Main content of the article',
+            publishedDate: '02-02-2023',
+            author: 'Bryan Anderson'
+        },
+    ],
 };
 
 export default initialState;
