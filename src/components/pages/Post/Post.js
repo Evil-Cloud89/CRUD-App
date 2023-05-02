cd224575e81692c33794d6879b83b91f224968bd
Mateusz Oko <mo89mo@gmail.com>
2023-05-02 23:42:29 +0200

    Render post, add Modal with remove post option

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,9 +1,62 @@
-const Post = () => {
+import { Link, Navigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getPostById, removePost } from "../../../redux/postsRedux";
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+const Post = props => {
+
+  const { id } = useParams();
+  const postData = useSelector(state => getPostById(state, id));
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true)
+
+
+  const dispatch = useDispatch();
+
+  const deletePost = () => {
+    dispatch(removePost(id));
+    handleClose();
+  };
+
+  if(!postData) return <Navigate to="/" />
     return (
-        <div>
-            <h1>Post</h1>
+      <div>
+        <div className="d-flex justify-content-between">
+          <h2>{postData.title}</h2>
+          <div>
+            <Link key={props.id} to={"/post/edit/" + id}>
+              <Button variant="outline-info m-1">Edit post</Button>
+            </Link>
+            <Button onClick={handleShow} variant="outline-danger m-1">Delete</Button>
+          </div>
         </div>
-    )
-}
+        <p><b>Author: </b>{postData.author}
+        <br/><b>Published: </b>{postData.publishedDate}</p>
+        <p>{postData.shortDescription}</p>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure?</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>This operation will completely remove this post from the app. 
+              <br/>Are you sure you want to do that?
+            </p>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={handleClose} variant="secondary">Cancel</Button>
+            <Button onClick={deletePost} variant="danger">Remove</Button>
+          </Modal.Footer>
+        </Modal>
+
+      </div>
+    );
+};
 
 export default Post;
