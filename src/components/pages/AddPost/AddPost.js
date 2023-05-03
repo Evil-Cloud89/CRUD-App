01fc0fc9c55347fc0aa99a895433d2ec83593a9e
Mateusz Oko <mo89mo@gmail.com>
2023-05-03 15:23:09 +0200

    Add AddPostForm

--- src/components/pages/AddPost/AddPost.js
+++ src/components/pages/AddPost/AddPost.js
@@ -1,7 +1,10 @@
+import AddPostForm from "../../features/AddPostForm/AddPostForm";
+
 const AddPost = () => {
     return (
         <div>
             <h1>AddPost</h1>
+            <AddPostForm />
         </div>
     )
 }
